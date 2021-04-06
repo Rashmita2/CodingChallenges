e8c436de627f07456f4caba50cd1e39533c7d7bb
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-04-05 20:04:16 -0500

    Add files via upload

---
+++ Java Challenges(linkedin)/Palindrome.java
@@ -0,0 +1,24 @@
+import java.util.stream.IntStream;
+import java.util.*;
+public class Palindrome {
+
+    public static boolean checkPalindrome(String text){
+        String newText = text.toLowerCase();
+        StringBuilder result = new StringBuilder();
+        
+        for(int i = newText.length() - 1; i>=0; i--){
+            result.append(newText.charAt(i));
+        }
+        return newText.equals(result.toString());
+        
+    }
+    public static void main(String[] args) {
+        String input = "racecar";
+        String input2 = "Hello";
+        String input3 = "aBba";
+        System.out.println(checkPalindrome(input));
+        System.out.println(checkPalindrome(input2));
+        System.out.println(checkPalindrome(input3));
+    }
+}
+    
